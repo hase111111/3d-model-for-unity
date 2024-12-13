a141059a7c8af75e31b0fc7ccff553eb18e4e787
Hasegawa Taisei <hasehasehase61@gmail.com>
2024-12-13 16:13:52 +0900

    Ostrichのアニメーションを追加

---
+++ Assets/CarryVault/Scrpts/Fantasy/OstrichAnimeRandomPlayer.cs
@@ -0,0 +1,39 @@
+
+#nullable enable
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CarryVault.Scripts.Fantasy
+{
+    public class OstrichAnimeRandomPlayer : MonoBehaviour
+    {
+        [SerializeField] float interval = 2.0f;
+        float _timer = 0.0f;
+
+        Animator? _animator;
+
+        void Start()
+        {
+            if (!TryGetComponent<Animator>(out _animator))
+            {
+                Debug.LogError("Animator is not found.");
+            }
+        }
+
+        void Update()
+        {
+            if (_animator == null) return;
+
+            _timer += Time.deltaTime;
+
+            if (_timer > interval)
+            {
+                bool is_walking = Random.Range(0, 2) == 0;
+                _timer = 0.0f;
+                _animator.SetBool("isWalking", is_walking);
+            }
+        }
+    }
+}
